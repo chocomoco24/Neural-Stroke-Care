acbb957dfc90ab7a9b0d61ed46b85a132512c954
chocomoco24 <debarghyadatta2004@gmail.com>
2026-05-07 21:24:30 +0530

    corrected next function and installed latest mongodb

--- backend/server.js
+++ backend/server.js
@@ -39,10 +39,9 @@ app.use((err, _req, res, _next) => {
 //Database + Start
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("[MongoDB] Connected");
+    console.log("[MongoDB] Connected: " + mongoose.connection.host);
     app.listen(PORT, () => console.log(`[Server] Running on port ${PORT}`));
   })
   .catch((err) => {
